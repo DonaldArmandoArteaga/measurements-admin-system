5d567c5fbaacef5cb9dc28d07ddaff5f54430854
Donald Armando Torres Arteaga <donald.torres@avista.co>
2022-12-25 18:49:01 -0500

    firt commit

---
+++ test/input-system.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InputSystem from '../lib/input-system-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/input-system-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InputSystem.InputSystemStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
