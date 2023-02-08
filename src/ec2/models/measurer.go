13e0da4e20508037489dd273680bf52b95e1126d
Donald Armando Torres Arteaga <donald.torres@avista.co>
2023-02-08 10:46:52 -0500

    changes in measurers

--- src/ec2/models/measurer.go
+++ src/ec2/models/measurer.go
@@ -3,15 +3,15 @@ package models
 import "time"
 
 type Measurement struct {
-	Id       string               `json:"id,omitempty"`
-	Serial   string               `json:"serial,omitempty"`
-	Date     time.Time            `json:"date,omitempty"`
-	Values   interface{}          `json:"values,omitempty"`
-	Metadata *MeasurementMetadata `json:"metadata,omitempty"`
+	Id       string               `json:"id,omitempty" dynamodbav:"id"`
+	Serial   string               `json:"serial,omitempty" dynamodbav:"serial"`
+	Date     time.Time            `json:"date,omitempty" dynamodbav:"date"`
+	Values   interface{}          `json:"values,omitempty" dynamodbav:"values"`
+	Metadata *MeasurementMetadata `json:"metadata,omitempty" dynamodbav:"metadata"`
 }
 
 type MeasurementMetadata struct {
-	DateInserted time.Time `json:"DateInserted,omitempty"`
+	DateInserted time.Time `json:"DateInserted,omitempty" dynamodbav:"DateInserted"`
 }
 
 type MeasurementFromDynamo struct {
